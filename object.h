5dbab890bd29ff65fe2faa2384354742046f8643
Daniel Oxman <forblaze@users.noreply.github.com>
2013-12-03 00:09:06 -0500

    Final version

--- object.h
+++ object.h
@@ -1,29 +1,17 @@
 #ifndef __OBJECT_H__
 #define __OBJECT_H__
 #include <string>
+#include "display.h"
 
-struct posn
-{
-	int row;
-	int col;
-	posn dirAdjacent(int dir);
-	int findDir(posn other);
-};
-
+// Structure used to package a race (HP, attack, defense, and a unique name)
 struct race
 {
 	int hp;
 	int atk;
 	int def;
 	std::string name;
+	bool operator==(race other); // Checks equality for races
 };
-
-bool operator==(race arg1, race arg2);
-bool operator==(posn arg1, posn arg2);
-
-const posn nullPosn = {-1, -1};
-
-const char FLOOR = '.';
 
 // Player races
 const race HUMAN = {140, 20, 20, "Human"};
@@ -40,9 +28,11 @@ const race GOBLIN = {70, 5, 10, "Goblin"};
 const race PHOENIX = {50, 35, 25, "Phoenix"};
 const race MERCHANT = {30, 70, 5, "Merchant"};
 const race DRAGON = {150, 20, 20, "Dragon"};
-const race EVILNINJA = {10, 100, 5, "Ninja"};
+const race EVILNINJA = {10, 100, 5, "Evil Ninja"};
 
 // Potion types (uses race struct for convenience)
+
+const race WATER = {0, 0, 0, "WATER"};	// Null (uninitialized) value for potions
 const race RH = {10, 0, 0, "RH"};
 const race BA = {0, 5, 0, "BA"};
 const race BD = {0, 0, 5, "BD"};
@@ -56,133 +46,145 @@ const int SMALL_HOARD = 2;
 const int MERCHANT_HOARD = 4;
 const int DRAGON_HOARD = 6;
 
-// Directions
-const int numDirections = 8;
-
-const int NORTHWEST = 0;
-const int NORTH = 1;
-const int NORTHEAST = 2;
-const int WEST = 3;
-const int EAST = 4;
-const int SOUTHWEST = 5;
-const int SOUTH = 6;
-const int SOUTHEAST = 7;
-// Object characters
-const char PLAYER = '@';
-const char STAIRS = '\\';
-const char POTION = 'P';
-const char GOLD = 'G';
-const char ENEMY = 'E';
-
+// Object superclass: Methods and fields common to all types of objects in the game
 class Object
 {
-	char objectChar;
-	posn loc;
+	char objectChar;			// Character that represents the object on the map
+	posn loc;					// Posn representing the object's coordinates on the grid
 protected:
 	void setObjectChar(char c); // Used by Enemy class to make different races look different
+	void setLoc(posn p); // Sets object location;
+						 // Protected to limit user control over object location
 public:
-	Object(char oC);
-	char getObjectChar();
-	posn getLoc();
-	void setLoc(posn p);
-	void initLoc(posn p); // Only works when loc is the null position; used to initialize posn;
-	virtual void die() = 0; // Might get rid of this? Hasn't actually been used yet...
+	Object(char oC);		// Constructor: Initializes object at nullPosn with given char
+	char getObjectChar();	// Returns character that represents the object
+	posn getLoc();		 	// Returns posn representing object's location
+	
+	void initLoc(posn p);   // Sets the object's location to given position;
+							// works ONLY if the object's position is currently null
+							// (ie. can only be called once per instance)
 };
 
+// Subclass of Object used for the stairs
 class Stairs:public Object
 {
 public:
-	Stairs();
-	void die();
+	Stairs(); // Simply creates an object with the '\' character
 };
 
+// Subclass of Object used for potions
 class Potion:public Object
 {
-	int hpMod;
-	int atkMod;
-	int defMod;
-	std::string type;
+	race pRace;				 // Race struct encapsulating potion modifiers and type
 public:
-	Potion(); // Makes a potion that does nothing; placeholder
-	void initPotion(race p); // Initializes empty potion; race structure used for potion mods
-	int getHP();
-	int getAtk();
-	int getDef();
-	std::string getType();
-	int getTypeNum();
-	void die();
+	Potion(); 				 // Creates an uninitialized potion ("WATER")
+	void initPotion(race p); // Provides values to an uninitialized potion
+	int getHP();			 // Returns potion's HP modifier
+	int getAtk();			 // Returns potion's Atk modifier
+	int getDef();			 // Returns potion's Def modifier
+	std::string getType();	 // Returns potion's type
+	int getTypeNum();		 // Returns integer corresponding to potion type (0-5)
 };
 
+// Subclass of Object used for gold
 class Gold:public Object
 {
-	int value;
-	bool guarded;
+	int value;				 // Integer representing the value of the gold pile
+	bool guarded;			 // Stores whether the gold is guarded
+							 // (true for dragon hoards while dragon is still alive,
+							 // false otherwise)
 public:
-	Gold();
-	void initValue(int v);
-	void setGuarded(bool guard);
-	bool isGuarded();
-	int getValue();
-	void die();
+	Gold();					 // Initializes gold to be unguarded with a value of 0
+	void initValue(int v);	 // If current value of the gold is 0, sets it to v
+	void setGuarded(bool guard); // Changes whether gold is guarded or not
+								 // Used with dragon hoards to make them guarded
+								 // when they are spawned and then make them
+								 // unguarded when the dragon is defeated
+	bool isGuarded();		 // Returns true if gold is guarded, false otherwise
+	int getValue();			 // Returns value of gold
 };
 
+// Subclass of Object; used as a superclass for both the player and enemies
 class Character:public Object
 {
-	int currentHP;
-	int currentAtk;
-	int currentDef;
+	int currentHP;	// Character's current HP
+	race cRace;		// Character's race
+public:
+	Character (char oC);  // Initializes the Character with given char and the null race
+	void move(int dir);   // Moves the character to an adjacent posn in a given direction
+	void setHP(int hp);   // Sets the character's current HP
+	int getHP();		  // Returns the character's current HP
+	std::string getName();// Returns the character's name, based on its race
+	virtual int getAtk(); // Returns the character's Atk, based on its race
+	virtual int getDef(); // Returns the character's Def, based on its race
+	race getRace();		  // Returns the character's race (max HP, Atk, Def, name)
+	void setRace(race r); // Sets the character's race
+};
+
+// Subclass of Character used for enemies
+class Enemy:public Character
+{
+	posn hoard;			  // Stores the location of the enemy's hoard (if it's a Dragon)
+						  // If the enemy is not a Dragon, this will equal nullPosn
+public:
+	Enemy();			  // Initializes an enemy to be a Dragon
+						  // This is done because Dragons are spawned by gold and thus
+						  // will not be spawned in the same way as other enemies;
+						  // therefore, the initRace() method will not be used for them
+	void initHoard(posn p); // Sets location of the enemy's hoard (if uninitialized)
+	posn hoardLoc();	    // Returns location of the enemy's hoard
+};
+
+// Subclass of Character used for the player
+class Player:public Character
+{
+	int currentAtk; // Player's current Atk
+	int currentDef; // Player's current Def
+	char tileChar;	// Character of the tile player is currently standing on
+					// this is necessary because players can move on more than just floor
+	double gold;	// Total gold player has picked up
+					// stored as a double because Orc players may pick up 0.5 gold
+	std::string message; // Stores a message to be printed to the user
+						 // message contents depend on the action the player took
+						 // and its degree of success
+	bool shinyDLC;	// Stores whether DLC is turned on or not;
+					// if it is, player may use the Ninja race, heal from phoenixes, etc
 protected:
 	void setAtk(int atk);
 	void setDef(int def);
 public:
-	Character (char oC);
-	void move(int dir);
-	void setHP(int hp);
-	int getHP();
-	int getAtk();
-	int getDef();
-};
-
-class Enemy:public Character
-{
-	race eRace;
-	posn hoard;
-public:
-	Enemy();
-	void initRace(race r);
-	std::string getRace();
-	void initHoard(posn p);
-	//void freeHoard();
-	posn hoardLoc();
-	void die();
-};
-
-class Player:public Character
-{
-	char tileChar;
-	race pRace;
-	double gold;
-	std::string message;
-	bool shinyDLC;
-public:
 	Player(); // Makes a default human player; placeholder
+	
+	// Accessor/mutator methods
+	int getAtk(); // Returns player's current Atk (may differ from race)
+	int getDef(); // Returns player's current Def (may differ from race)
 	char getTileChar();
 	void setTileChar(char c);
 	double getGold();
 	void setMessage(std::string m);
 	std::string getMessage();
-	std::string getRace();
-	void initGold(double g); // Only works when gold is at 0; sets last floor's value
-	void initHP(int hp);   // Only works when hp is at race total; sets last floor's value
-	void initRace(char c); // Initializes race from char; used when race is read in at start
-	void initDLC(bool dlc);
-	void addGold(double value);
-	void usePotion (Potion &p);
-	void oxmanly(); //Cheats are fun!
-	void grosslyOverpowered(); //Hooray!
-	void die();
-	bool attack(Enemy * e); //Returns true if the attack kills the enemy
-	bool getAttacked(Enemy * e); //Returns true if the attack kills the player
+	
+	// Initializer methods; used to set up the player object when a new floor begins
+	void initGold(double g);// Only works when gold is at 0; 
+							// sets gold to last floor's value
+	void initHP(int hp);    // Only works when hp is at race total;
+							// sets last floor's value
+	void initRace(char c); 	// Initializes race from char;
+							// used when race is read in at start
+	void initDLC(bool dlc); // Initializes DLC variable
+	
+	// Item-related methods
+	void addGold(double value);	// Adds value of a gold pile to the player
+	void usePotion (Potion &p); // Applies the modifiers of a potion the player
+	
+	// Cheats (yay!)
+	void oxmanly();				// Multiplies Def by 9001
+	void grosslyOverpowered();  // Multiplies Atk by 144
+	
+	// Attack/damage related methods
+	bool attack(Enemy * e); // Modifies enemy HP;
+							// Returns true if the attack kills the enemy
+	bool getAttacked(Enemy * e); // Returns true if the attack kills the player
 };
 
 #endif
